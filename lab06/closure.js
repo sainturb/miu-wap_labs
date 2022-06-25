690a8ad2a23c6a945fd557882b9e98f52d8708c8
saintur_b <saintur.b@novelsoft.mn>
2022-06-25 13:02:05 -0500

    up

--- lab06/closure.js
+++ lab06/closure.js
@@ -2,7 +2,7 @@ function printNumbers(from, to) {
   'use strict';
   let interval = setInterval(() => {
     document.getElementById('exercise1').innerText = from;
-    if (from == to + 1) {
+    if (from == parseInt(to, 0) + 1) {
       document.getElementById('exercise1').innerText = 'Finished';
       clearInterval(interval);
     }
