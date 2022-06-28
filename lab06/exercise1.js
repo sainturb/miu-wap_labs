15408fa8b7b9d6ce86021be315c2b493a1f600b5
saintur_b <saintur.b@novelsoft.mn>
2022-06-28 11:57:56 -0500

    fixes

--- lab06/exercise1.js
+++ lab06/exercise1.js
@@ -1,13 +1,10 @@
 // function makeArmy() {
 //   let shooters = [];
-//   // let i = 0;
-//   // while(i < 2) {
 //     for(let i = 0; i<2; i++) {
 //     let shooter = function() {
 //       console.log(i);
 //     }
 //     shooters.push(shooter);
-//     // i++;
 //   }
 //   return shooters;
 // }
