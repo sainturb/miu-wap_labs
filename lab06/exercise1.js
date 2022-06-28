520748a3399ac10a74c0197b41473192d641425c
saintur_b <saintur.b@novelsoft.mn>
2022-06-28 11:53:50 -0500

    fix on EC and LE

--- lab06/exercise1.js
+++ lab06/exercise1.js
@@ -1,16 +1,31 @@
+// function makeArmy() {
+//   let shooters = [];
+//   // let i = 0;
+//   // while(i < 2) {
+//     for(let i = 0; i<2; i++) {
+//     let shooter = function() {
+//       console.log(i);
+//     }
+//     shooters.push(shooter);
+//     // i++;
+//   }
+//   return shooters;
+// }
+// let army = makeArmy();
+
 function makeArmy() {
   let shooters = [];
-  // let i = 0;
-  // while(i < 2) {
-    for(let i = 0; i<2; i++) {
+  let i = 0;
+  while(i < 2) {
+    let j = i;
     let shooter = function() {
-      console.log(i);
+      console.log(j)
     }
     shooters.push(shooter);
-    // i++;
+    i++;
   }
   return shooters;
 }
 let army = makeArmy();
-
-army.forEach(a => a());
+army[0]();
+// army.forEach(a => a());
