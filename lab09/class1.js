c77cacdac86c4caf658043a069f298a818790b6f
saintur_b <saintur.b@novelsoft.mn>
2022-07-01 08:38:09 -0500

    9 added

--- lab09/class1.js
+++ lab09/class1.js
@@ -1,34 +1,67 @@
 
 class LinkedList {
-  
-  constructor() {
+
+  constructor(arr) {
+    if (arr) {
+      arr.forEach(item => this.add(item));
+    }
   }
 
   add(value) {
-    //  this.push(value);
-  }
-  
-  remove(value) {
-    // this.splice(this.indexOf(value), 1);
-  }
-  
-  print() {
-    console.log(this);
+    if (this.value === undefined) {
+      this.value = value;
+      this.next = null;
+    } else {
+      let current = this;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = { value: value, next: null };
+    }
   }
 
+  remove(value) {
+    var current = this;
+    var prev = null;
+    while (current) {
+      if (current.value === value) {
+        if (prev == null) {
+          this.value = current.next.value;
+          this.next = current.next.next;
+        } else {
+          prev.next = current.next;
+        }
+        return true;
+      }
+      prev = current;
+      current = current.next;
+    }
+    return false;
+  }
+
+  print() {
+    let result = 'LinkedList{';
+    result = this.helper(this, result);
+    result += '}\n';
+    const console1 = document.getElementById('console1');
+    console1.innerText += result;
+    console.log(result);
+  }
+
+  helper(list, result) {
+    if (list.next == null) {
+      result += list.value;
+      return result;
+    }
+    result += list.value + ',';
+    return this.helper(list.next, result);
+  }
 }
 
 var linkedlist = new LinkedList();
-// linkedlist.add(1);
-// linkedlist.add(2);
-// linkedlist.add(3);
-// linkedlist.print();
-// linkedlist.remove(3);
-linkedlist.print();
-
-// linkedlist.add(1);
-// linkedlist.add(2);
-// linkedlist.add(3);
-// linkedlist.print(); //in the console, you should see: LinkedList{1,2,3}
-// linkedlist.remove(3);
-// linkedlist.print(); //in the console, you should see: LinkedList{1,3}
+linkedlist.add(1);
+linkedlist.add(2);
+linkedlist.add(3);
+linkedlist.print(); //in the console, you should see: LinkedList{1,2,3}
+linkedlist.remove(3);
+linkedlist.print(); //in the console, you should see: LinkedList{1,3}
