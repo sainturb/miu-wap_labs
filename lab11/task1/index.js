74099b1e34855087535f22c4f5d4cf28f4093997
saintur_b <saintur.b@novelsoft.mn>
2022-07-05 12:07:56 -0500

    host printable

--- lab11/task1/index.js
+++ lab11/task1/index.js
@@ -1,14 +1,12 @@
 const dns = require('node:dns');
-
-dns.lookup('www.miu.edu', (err, address, family) => {
-  console.log('address: %j family: IPv%s', address, family);
+const host1 = 'www.facebook.com';
+dns.lookup(host1, (err, address, family) => {
+  console.log(`address: %j family: IPv%s of ${host1}`, address, family);
 });
-
-dns.resolve4('www.miu.edu', (err, addresses) => {
+const host2 = 'www.miu.edu';
+dns.resolve4(host2, (err, addresses) => {
   if (err) throw err;
-
-  console.log(`addresses: ${JSON.stringify(addresses)}`);
-
+  console.log(`addresses: ${JSON.stringify(addresses)} of ${host2}`);
   addresses.forEach((a) => {
     dns.reverse(a, (err, hostnames) => {
       if (err) {
